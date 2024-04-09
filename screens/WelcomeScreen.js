93ddcd944416519e0a9aaad167698891e136cdcd
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-09 14:18:50 -0700

    Imported statring little lemon project code, up and running on phone

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const WelcomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Iraishaimase</Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
